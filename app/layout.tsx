708e15ee5be96ab0e1fabce3dde818932345359c
Marshall Acharibia <macharibia@npontu.com>
2026-03-23 02:31:53 +0000

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,8 +49,8 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="en" className={`${cabin.variable} ${caprasimo.variable} bg-neutral-light dark:bg-brand-darker antialiased`}>
       <body className={abeezee.variable} suppressHydrationWarning>
         <Script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
-          strategy="beforeInteractive"
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&loading=async`}
+          strategy="afterInteractive"
         />
         <QueryProvider>
         <RouterInitializer />
